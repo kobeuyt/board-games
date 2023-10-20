537e60db7f000afddb63d8dac07590c66619468f
Kobe Uytiepo <kobeuyt@gmail.com>
2023-10-20 13:59:21 -0400

    Added initial TicTacToe code

--- tictactoe/src/Board.cpp
+++ tictactoe/src/Board.cpp
@@ -1,109 +1,126 @@
-#include "../include/Board.h"
+#include "Board.h"
 #include <cstdio>
 #include <string>
 
-using namespace games;
-
-Board::Board()
+namespace games
 {
-    board = new std::map<Position, Player>();
-    for (Position p : POS_ARR)
+    Board::Board()
     {
-        board->at(p) = Player::EMPTY;
+        board = new std::map<Position, Player>();
+        for (Position p : POS_ARR)
+        {
+            board->insert({p, Player::EMPTY});
+        }
+    }
+
+    Board::~Board()
+    {
+        delete board;
+    }
+
+    void Board::Move(Position p, Player pl)
+    {
+        if (ValidMove(p))
+        {
+            board->at(p) = pl; 
+        }
+    }
+
+    bool Board::ValidMove(Position p)
+    {
+        if (board->at(p) == Player::EMPTY)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    int Board::MovesLeft()
+    {
+        return GetValidMoves().size();
+    }
+
+    std::vector<Position> Board::GetValidMoves()
+    {
+        std::vector<Position> validMoves = std::vector<Position>();
+
+        for (Position p : POS_ARR)
+        {
+            if (ValidMove(p)) {validMoves.push_back(p);}
+        }
+
+        return validMoves;
+    }
+
+    Player Board::CheckWinner()
+    {
+        // Top row winner
+        if (board->at(Position::TOP_LEFT) == board->at(Position::TOP_MIDDLE)
+        && board->at(Position::TOP_MIDDLE) == board->at(Position::TOP_RIGHT))
+        {
+            return board->at(Position::TOP_LEFT);
+        }
+        // Middle row winner
+        else if (board->at(Position::MIDDLE_LEFT) == board->at(Position::CENTER) 
+        && board->at(Position::CENTER) == board->at(Position::MIDDLE_RIGHT))
+        {
+            return board->at(Position::MIDDLE_LEFT);
+        }
+        // Bottom row winner
+        else if (board->at(Position::BOTTOM_LEFT) == board->at(Position::BOTTOM_MIDDLE) 
+        && board->at(Position::BOTTOM_MIDDLE) == board->at(Position::BOTTOM_RIGHT))
+        {
+            return board->at(Position::BOTTOM_LEFT);
+        }
+        // Top left to bottom right diagonal winner
+        else if (board->at(Position::TOP_LEFT) == board->at(Position::CENTER) 
+        && board->at(Position::CENTER) == board->at(Position::BOTTOM_RIGHT))
+        {
+            return board->at(Position::TOP_LEFT);
+        }
+        // Top right to bottom left diagonal winner
+        else if (board->at(Position::TOP_RIGHT) == board->at(Position::CENTER) 
+        && board->at(Position::CENTER) == board->at(Position::BOTTOM_LEFT))
+        {
+            return board->at(Position::TOP_RIGHT);
+        }
+        // Left column winner
+        else if (board->at(Position::TOP_LEFT) == board->at(Position::MIDDLE_LEFT) 
+        && board->at(Position::MIDDLE_LEFT) == board->at(Position::BOTTOM_LEFT))
+        {
+            return board->at(Position::TOP_LEFT);
+        }
+        // Middle column winner
+        else if (board->at(Position::TOP_MIDDLE) == board->at(Position::CENTER) 
+        && board->at(Position::CENTER) == board->at(Position::BOTTOM_MIDDLE))
+        {
+            return board->at(Position::TOP_MIDDLE);
+        }
+        // Right column winner
+        else if (board->at(Position::TOP_RIGHT) == board->at(Position::MIDDLE_RIGHT) 
+        && board->at(Position::MIDDLE_RIGHT) == board->at(Position::BOTTOM_RIGHT))
+        {
+            return board->at(Position::TOP_RIGHT);
+        }
+        else
+        {
+            return Player::EMPTY;
+        }
+    }
+
+    void Board::PrintBoard()
+    {
+        std::printf("\n");
+        std::printf("     |     |     \n");
+        std::printf("  %c  |  %c  |  %c  \n", PLAYER_CHAR.at(board->at(Position::TOP_LEFT)), PLAYER_CHAR.at(board->at(Position::TOP_MIDDLE)), PLAYER_CHAR.at(board->at(Position::TOP_RIGHT)));
+        std::printf("_____|_____|_____\n");
+        std::printf("     |     |     \n");
+        std::printf("  %c  |  %c  |  %c  \n", PLAYER_CHAR.at(board->at(Position::MIDDLE_LEFT)), PLAYER_CHAR.at(board->at(Position::CENTER)), PLAYER_CHAR.at(board->at(Position::MIDDLE_RIGHT)));
+        std::printf("_____|_____|_____\n");
+        std::printf("     |     |     \n");
+        std::printf("  %c  |  %c  |  %c  \n", PLAYER_CHAR.at(board->at(Position::BOTTOM_LEFT)), PLAYER_CHAR.at(board->at(Position::BOTTOM_MIDDLE)), PLAYER_CHAR.at(board->at(Position::BOTTOM_RIGHT)));
+        std::printf("     |     |     \n");
+        std::printf("\n");
     }
 }
 
-Board::~Board()
-{
-    delete board;
-}
-
-void Board::Move(Position p, Player pl)
-{
-    if (ValidMove(p))
-    {
-        board->at(p) = pl; 
-    }
-}
-
-bool Board::ValidMove(Position p)
-{
-    if (board->at(p) == Player::EMPTY)
-    {
-        return true;
-    }
-    return false;
-}
-
-int Board::MovesLeft()
-{
-    int emptySpaces = 0;
-
-    for (Position p : POS_ARR)
-    {
-        if (board->at(p) == Player::EMPTY) {emptySpaces++;}    
-    }
-
-    return emptySpaces;
-}
-
-Player Board::CheckWinner()
-{
-    if (board->at(Position::TOP_LEFT) == board->at(Position::TOP_MIDDLE)
-     && board->at(Position::TOP_MIDDLE) == board->at(Position::TOP_RIGHT))
-    {
-        return board->at(Position::TOP_LEFT);
-    }
-    else if (board->at(Position::MIDDLE_LEFT) == board->at(Position::CENTER) 
-     && board->at(Position::CENTER) == board->at(Position::MIDDLE_RIGHT))
-    {
-        return board->at(Position::MIDDLE_LEFT);
-    }
-    else if (board->at(Position::BOTTOM_LEFT) == board->at(Position::BOTTOM_MIDDLE) 
-     && board->at(Position::BOTTOM_MIDDLE) == board->at(Position::BOTTOM_RIGHT))
-    {
-        return board->at(Position::BOTTOM_LEFT);
-    }
-    else if (board->at(Position::MIDDLE_LEFT) == board->at(Position::CENTER) 
-     && board->at(Position::CENTER) == board->at(Position::BOTTOM_RIGHT))
-    {
-        return board->at(Position::MIDDLE_LEFT);
-    }
-    else if (board->at(Position::MIDDLE_RIGHT) == board->at(Position::CENTER) 
-     && board->at(Position::CENTER) == board->at(Position::BOTTOM_LEFT))
-    {
-        return board->at(Position::MIDDLE_RIGHT);
-    }
-    else if (board->at(Position::TOP_LEFT) == board->at(Position::MIDDLE_LEFT) 
-     && board->at(Position::MIDDLE_LEFT) == board->at(Position::BOTTOM_LEFT))
-    {
-        return board->at(Position::TOP_LEFT);
-    }
-    else if (board->at(Position::TOP_MIDDLE) == board->at(Position::CENTER) 
-     && board->at(Position::CENTER) == board->at(Position::BOTTOM_MIDDLE))
-    {
-        return board->at(Position::TOP_MIDDLE);
-    }
-    else if (board->at(Position::TOP_RIGHT) == board->at(Position::MIDDLE_RIGHT) 
-     && board->at(Position::MIDDLE_RIGHT) == board->at(Position::BOTTOM_RIGHT))
-    {
-        return board->at(Position::TOP_RIGHT);
-    }
-    else
-    {
-        return Player::EMPTY;
-    }
-}
-
-void Board::PrintBoard()
-{
-    std::printf("     |     |     ");
-    std::printf("  %c  |  %c  |  %c  ", PLAYER_CHAR.at(board->at(Position::TOP_LEFT)), PLAYER_CHAR.at(board->at(Position::TOP_MIDDLE)), PLAYER_CHAR.at(board->at(Position::TOP_RIGHT)));
-    std::printf("_____|_____|_____");
-    std::printf("     |     |     ");
-    std::printf("  %c  |  %c  |  %c  ", PLAYER_CHAR.at(board->at(Position::MIDDLE_LEFT)), PLAYER_CHAR.at(board->at(Position::CENTER)), PLAYER_CHAR.at(board->at(Position::MIDDLE_RIGHT)));
-    std::printf("_____|_____|_____");
-    std::printf("     |     |     ");
-    std::printf("  %c  |  %c  |  %c  ", PLAYER_CHAR.at(board->at(Position::BOTTOM_LEFT)), PLAYER_CHAR.at(board->at(Position::BOTTOM_MIDDLE)), PLAYER_CHAR.at(board->at(Position::BOTTOM_RIGHT)));
-    std::printf("     |     |     ");
-}
